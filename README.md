0cdaaace0bb296a235112fc839098ff77ae895ec
Sergey <karandeev73@gmail.com>
2022-02-12 18:05:16 +0300

    feat: add paint.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Kivy python
 1. Калькулятор
 2. Крестики-нолики
+3. Paint and save
+4. 
---
+++ paint.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+from random import random
+
+from kivy.core.window import Window
+from kivy.graphics import (Color, Ellipse, Rectangle, Line)
+
+class PainterWidget(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(random(), random(), random(), 1)
+            radius  = 30
+            Ellipse(pos = (touch.x - radius/2, touch.y - radius/2), size = (radius, radius))
+            touch.ud['line'] = Line(points = (touch.x, touch.y), width = radius/2)
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += (touch.x, touch.y)
+
+class PaintApp(App):
+    def build(self):
+        parent = Widget()
+        self.painter = PainterWidget()
+
+        parent.add_widget(self.painter)
+
+        parent.add_widget(Button(text = 'Clear', on_press = self.clear_canvas, size = (100, 50)))
+        parent.add_widget(Button(text='Save', on_press = self.save, size=(100, 50), pos = (100, 0)))
+        parent.add_widget(Button(text='Screen', on_press=self.screen, size=(100, 50), pos=(200, 0)))
+        return parent
+
+    def clear_canvas(self, instance):
+        self.painter.canvas.clear()
+
+    def save(self, instance):
+        self.painter.size= (Window.size[0], Window.size[1])
+        self.painter.export_to_png('image.png')
+
+    def screen(self, instance):
+        Window.screenshot('screen.png')
+
+
+if __name__ == "__main__":
+    PaintApp().run()
